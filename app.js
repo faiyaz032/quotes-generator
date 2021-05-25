242d16386af803fcc8300524a43c9bfeb0ea8e41
Faiyaz Rahman <faiyazrahman03@gmail.com>
2021-05-25 18:16:21 +0600

    completed initial functionalities

--- app.js
+++ app.js
@@ -5,4 +5,24 @@
  * Date: 5/25/2021
  */
 
+const mathLibrary = require(`${__dirname}/lib/math`);
+const quotesLibrary = require(`${__dirname}/lib/quotes`);
+
 const app = {};
+
+app.config = {
+   timeBeetweenQuotes: 2000,
+};
+
+app.printQuotes = function printQuotes() {
+   const allQuotes = quotesLibrary.generateQuote();
+   const numberOfQuotes = allQuotes.length;
+   const randomNumber = mathLibrary.generateRandomNumber(1, numberOfQuotes);
+   console.log(allQuotes[randomNumber - 1]);
+};
+
+app.indifiniteQuoteLoop = function indifiniteQuoteLoop() {
+   setInterval(this.printQuotes, app.config.timeBeetweenQuotes);
+};
+
+app.indifiniteQuoteLoop();
